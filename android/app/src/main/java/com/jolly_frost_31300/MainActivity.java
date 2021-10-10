e2e9c411acc1cdb1e8477daf003645e85716a2ee
Crowdbotics <team@crowdbotics.com>
2021-10-10 21:52:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/jolly_frost_31300/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jolly_frost_31300;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "jolly_frost_31300";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "jolly_frost_31300",
+  url: "https://jolly_frost_31300.botics.co",
+  api: "https://jolly_frost_31300.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
